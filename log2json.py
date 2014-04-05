1855df5bffd87b73fa6208d7bc79353421524c44
ben-bougher <ben.bougher@gmail.com>
2014-04-05 14:18:02 -0500

    added decimation to json output

--- log2json.py
+++ log2json.py
@@ -6,10 +6,11 @@ las_reader = las.LASReader(las_file)
 
 output_file = 'www/log_json.json'
 data = las_reader.data
+decimate = 25
 
 las_dict = {}
 for name in data.dtype.names:
-    las_dict[name] = data[name].tolist()
+    las_dict[name] = data[name][::25].tolist()
 
 json_out = json.dumps(las_dict)
 
